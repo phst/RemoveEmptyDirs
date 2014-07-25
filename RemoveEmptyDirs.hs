9de08279b22adfa33c57b91837e87f8a04973676
Philipp Stephani <p.stephani2@gmail.com>
2014-07-25 13:52:42 +0200

    add more logging

--- RemoveEmptyDirs.hs
+++ RemoveEmptyDirs.hs
@@ -10,29 +10,37 @@
 module RemoveEmptyDirs (run, setLogLevel) where
 
 import Control.Applicative ((<$>))
+import Control.Monad (when)
 import System.Directory (doesDirectoryExist, getDirectoryContents, removeDirectory, removeFile)
 import System.FilePath ((</>))
 import System.IO.Error (tryIOError)
-import System.Log.Logger (Priority, errorM, setLevel, updateGlobalLogger)
+import System.Log.Logger (Priority, debugM, errorM, infoM, setLevel, updateGlobalLogger)
 
 run :: [FilePath] -> IO ()
 run = mapM_ traverse
 
 traverse :: FilePath -> IO Entry
 traverse dir = do
+  debug $ "traversing directory " ++ dir
   let process "." = return Ignore
       process ".." = return Ignore
-      process name@".DS_Store" = return $ JunkFile (dir </> name)
+      process name@".DS_Store" = do
+        let path = dir </> name
+        debug $ "processing junk file " ++ path
+        return $ JunkFile path
       process name = do
         let path = dir </> name
             processDir = do
+              debug $ "processing directory " ++ path
               res <- tryIOError $ traverse path
               case res of
                 Right r -> return r
                 Left ex -> do
                   warn ex
                   return $ Directory path
-            processFile = return $ PreciousFile path
+            processFile = do
+              debug $ "processing precious file " ++ path
+              return $ PreciousFile path
         isDir <- doesDirectoryExist path
         if isDir then processDir else processFile
   contents <- getDirectoryContents dir >>= mapM process
@@ -41,23 +49,33 @@ traverse dir = do
       junkFile (JunkFile _) = True
       junkFile _ = False
       entries = filter (not . ignore) contents
-      removeJunk =
+      removeJunk = do
+        debug $ "removing junk files in directory " ++ dir
         let remove (JunkFile f) = do
+              debug $ "removing junk file " ++ f
               res <- tryIOError $ removeFile f
               case res of
-                Right _ -> return True
+                Right _ -> do
+                  info $ "removed junk file " ++ f
+                  return True
                 Left ex -> do
                   warn ex
                   return False
             remove _ = error "remove applied to invalid entry"
-        in and <$> mapM remove entries
-      returnDir = return $ Directory dir
+        and <$> mapM remove entries
+      returnDir = do
+        debug $ "directory " ++ dir ++ " is not empty"
+        return $ Directory dir
       removeDir = do
         success <- removeJunk
+        when success $ info $ "removed all junk files in directory " ++ dir
         let remove = do
+              debug $ "removing empty directory " ++ dir
               res <- tryIOError $ removeDirectory dir
               case res of
-                Right _ -> return Ignore
+                Right _ -> do
+                  info $ "removed empty directory " ++ dir
+                  return Ignore
                 Left ex -> do
                   warn ex
                   returnDir
@@ -71,6 +89,14 @@ data Entry = Ignore
 
 warn :: IOError -> IO ()
 warn ex = errorM loggerName $ show ex
+err :: String -> IO ()
+err = errorM loggerName
+
+info :: String -> IO ()
+info = infoM loggerName
+
+debug :: String -> IO ()
+debug = debugM loggerName
 
 setLogLevel :: Priority -> IO ()
 setLogLevel prio = updateGlobalLogger loggerName $ setLevel prio
